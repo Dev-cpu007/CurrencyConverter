9772501e501d302fc4ef49363111296795cc9889
Devkinandan Sharma <2020bcs014@sggs.ac.in>
2025-04-26 21:45:27 +0530

    create swap function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import UseCurrencyInfo from "./hooks/UseCurrencyInfo.js"
+import InputBox from './components/InputBox.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount,setAmount] = useState(0)
+  const [from,setFrom] = useState("usd")
+  const[to,setTo] = useState("inr")
+  const [convertedAmount,setConvertedAmount] = useState(0)
 
+  const currencyInfo = UseCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+
+  const swap = ()=>{
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
   return (
     <>
-     <h1 class="text-3xl font-bold underline">
-      
-    </h1>
+    
     </>
   )
 }
