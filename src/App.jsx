1c8e08cc24bd6f2449323f0eaa1907e167dd22e3
Devkinandan Sharma <2020bcs014@sggs.ac.in>
2025-04-26 20:01:55 +0530

    Input box component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UseCurrencyInfo from "./hooks/UseCurrencyInfo.js"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <h1 class="text-3xl font-bold underline">
+      
+    </h1>
     </>
   )
 }
